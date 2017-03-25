5ff85c660b6f7c04e846c2c6bca8127f825543e1
Prash <prash@pelucid.com>
2017-03-25 23:13:41 +0000

    initial components added

---
+++ TunerButton.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { View,
+         TouchableHighlight,
+         Text,
+         StyleSheet
+        } from 'react-native';
+
+export default class TunerButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pressing: false };
+  }
+
+  _onPress = () => {
+    this.setState({ pressing: true });
+    console.log("pressed");
+  }
+
+  _onRelease = () => {
+    this.setState({ pressing: false });
+    console.log("released");
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight style={styles.touchable}
+                            onPressIn={this._onPress}
+                            onPressOut={this._onRelease}>
+          <View style={styles.button}>
+            <Text style={styles.label}>{this.state.pressing ? 'E' : 'E!'}</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  touchable: {
+    borderRadius: 25
+  },
+  label:{
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#FFFFFF'
+  },
+  button: {
+    backgroundColor: '#880000',
+    borderRadius: 25,
+    height: 50,
+    width: 50,
+    justifyContent: 'center'
+  }
+});
