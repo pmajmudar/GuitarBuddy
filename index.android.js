bdb212daa8529b4575be8e489fb4f236014ea608
Prash <prash@pelucid.com>
2017-03-26 22:47:45 +0100

    add toolbar initial code

--- index.android.js
+++ index.android.js
@@ -5,8 +5,36 @@
  */
 
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import GuitarBuddy from './GuitarBuddy';
 
+export default class ToolBar extends Component {
 
-AppRegistry.registerComponent('GuitarBuddy', () => GuitarBuddy);
+  render() {
+    return (
+        <Icon.ToolbarAndroid style={styles.toolbar}
+          title='GuitarBuddy'
+          titleColor="white"
+          subtitle='Tuner'
+          navIconName="navicon"
+          //onIconClicked={props.navigator.pop}
+          actions={[
+            { title: 'Settings', iconName: 'cog', iconSize: 30, show: 'always' },
+            { title: 'About', iconName: 'cog', iconSize: 30, show: 'ifroom' }
+          ]}
+          overflowIconName="ellipsis-v"
+        />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  toolbar: {
+    height: 56,
+    backgroundColor: 'steelblue'
+  },
+});
+
+
+AppRegistry.registerComponent('GuitarBuddy', () => ToolBar);
